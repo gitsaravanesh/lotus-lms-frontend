cd8a4bb4cef3768cc25687c3422114c47dedeb88
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-17 01:14:08 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Signup from "./components/signup";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
