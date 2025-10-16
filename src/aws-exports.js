b0e603e9a6e6e9cd8de55e5ab4cddd0f0db1b22d
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-16 16:44:47 +0530

    build after cf

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,7 @@ const awsConfig = {
     userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
     userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
     oauth: {
-      domain: process.env.REACT_APP_COGNITO_DOMAIN,
+      domain: process.env.REACT_APP_COGNITO_DOMAIN.replace('https://', ''),
       scope: ["email", "openid", "profile"],
       redirectSignIn: process.env.REACT_APP_REDIRECT_URL,
       redirectSignOut: process.env.REACT_APP_REDIRECT_URL,
