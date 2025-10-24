73b0a2b99f84304f17ee0e4a6bfdba17c36e6e87
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-24 11:15:28 +0530

    fix signup

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,3 +1,3 @@
-export const CLIENT_ID = "1gd98lgt6jqtletgio0e2us33n";
+export const CLIENT_ID = "49gusp4sidkggc371vghtdvujb";
 export const COGNITO_DOMAIN = "https://lms-auth-dev-sarav.auth.ap-south-1.amazoncognito.com";
 export const REDIRECT_URI = "https://dodyqytcfhwoe.cloudfront.net/";
